ffe36ab8c9939bb9fd2256f036c97853bdc241f1
gsanchexs <gabrielcfhabbo@gmail.com>
2024-10-03 12:15:55 +0000

    Apache Server v1.0

---
+++ IaC_webServer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Criando WebServer..."
+
+
+echo "Atualizando a VM..."
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+
+cp -R * /var/www/html
+
+echo "WebServer criado!"
